f203f20ea547cf592a96972f6118189b4e246c30
Lucas Nunno <lucasnunno@gmail.com>
2017-01-19 18:46:39 -0700

    Strip whitespace

--- alias.d/git_aliases.zsh
+++ alias.d/git_aliases.zsh
@@ -12,9 +12,9 @@ alias gcv="git commit -v"
 alias gco="git checkout"
 alias gm="git merge"
 alias gd="git diff"
-alias gpo='git push --set-upstream origin'                                                                                                                                                                                                               
+alias gpo='git push --set-upstream origin'
 alias gporigin='git push --set-upstream origin'
 alias git-prune-branches='git branch --merged | grep -v "\*" | grep -Ev "(\*|master|develop|staging)" | xargs -n 1 git branch -d'
-                                                                                                                                                                                                                                                              
-# Git push my branch to origin's branch of the same name.                                                                                                                                                                                                     
+
+# Git push my branch to origin's branch of the same name.
 alias git-push-upstream='git push --set-upstream origin $(git symbolic-ref --short HEAD)'
--- alias.d/mvn_aliases.sh
+++ alias.d/mvn_aliases.sh
@@ -1,15 +1,15 @@
-# See: https://github.com/Bash-it/bash-it/blob/master/aliases/available/maven.aliases.bash                                                                                                                                                                    
-                                                                                                                                                                                                                                                              
-alias m='mvn'                                                                                                                                                                                                                                                 
-alias mc='mvn compile'                                                                                                                                                                                                                                        
-alias mcc='mvn clean compile'                                                                                                                                                                                                                                 
-alias mdocs="mvn dependency:sources dependency:resolve -Dclassifier=javadoc"                                                                                                                                                                                  
-alias mclean='mvn clean'                                                                                                                                                                                                                                      
-alias mci='mvn clean install'                                                                                                                                                                                                                                 
-alias mi='mvn install'                                                                                                                                                                                                                                        
-alias mrprep='mvn release:prepare'                                                                                                                                                                                                                            
-alias mrperf='mvn release:perform'                                                                                                                                                                                                                            
-alias mrrb='mvn release:rollback'                                                                                                                                                                                                                             
-alias mdep='mvn dependency:tree'                                                                                                                                                                                                                              
-alias mpom='mvn help:effective-pom'                                                                                                                                                                                                                           
-alias mcisk='mci -Dmaven.test.skip=true'    
+# See: https://github.com/Bash-it/bash-it/blob/master/aliases/available/maven.aliases.bash
+
+alias m='mvn'
+alias mc='mvn compile'
+alias mcc='mvn clean compile'
+alias mdocs="mvn dependency:sources dependency:resolve -Dclassifier=javadoc"
+alias mclean='mvn clean'
+alias mci='mvn clean install'
+alias mi='mvn install'
+alias mrprep='mvn release:prepare'
+alias mrperf='mvn release:perform'
+alias mrrb='mvn release:rollback'
+alias mdep='mvn dependency:tree'
+alias mpom='mvn help:effective-pom'
+alias mcisk='mci -Dmaven.test.skip=true'
