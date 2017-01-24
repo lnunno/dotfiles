9e5ab591b434d5d6ca239f8e450e5f1f7aaf3098
Lucas Nunno <lucasnunno@gmail.com>
2017-01-23 22:49:40 -0700

    Update aliases.zsh

--- alias.d/aliases.zsh
+++ alias.d/aliases.zsh
@@ -1,9 +1,7 @@
-alias v="vim"
-alias abs="realpath"
-alias abspath="realpath"
+alias v='vim'
+alias abs='realpath'
+alias abspath='realpath'
+alias mysudo='sudo env PATH=$PATH'
 
-cdf(){
-    cd $(dirname $1)
-}
-
+cdf(){ cd $(dirname $1) }
 cdfile(){ cdf $1}
