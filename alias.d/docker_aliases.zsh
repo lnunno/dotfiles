1e66991968600a2dd1fbb4a0e3d281aa5caadb2e
Lucas Nunno <lucasnunno@gmail.com>
2017-03-08 17:20:40 -0700

    Update docker_aliases.zsh

--- alias.d/docker_aliases.zsh
+++ alias.d/docker_aliases.zsh
@@ -1,2 +1,16 @@
 alias docker-clean-exited-containers='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
 alias docker-clean-dangling-images='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
+
+alias dck='docker'
+alias di='docker images'
+
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+
+alias drm='docker rm'
+alias drmi='docker rmi'
+
+alias dex='docker exec'
+alias dexit='docker exec -it'
+alias drun='docker run'
+alias drunit='docker run -it'
