5b1dfc8083639816e8accc7bd909e4b115ed6f73
Lucas Nunno <lucasnunno@gmail.com>
2017-04-12 18:08:54 -0600

    Update and rename aliases.zsh to aliases.sh

---
+++ alias.d/aliases.sh
@@ -0,0 +1,11 @@
+alias v='vim'
+alias abs='realpath'
+alias abspath='realpath'
+alias mysudo='sudo env PATH=$PATH'
+
+cdf(){ cd $(dirname $1) }
+cdfile(){ cdf $1}
+
+# See: http://stackoverflow.com/questions/749544/pipe-to-from-the-clipboard
+alias setclip='xclip -selection c'
+alias getclip='xclip -selection clipboard -o'
--- alias.d/aliases.zsh
+++
@@ -1,7 +0,0 @@
-alias v='vim'
-alias abs='realpath'
-alias abspath='realpath'
-alias mysudo='sudo env PATH=$PATH'
-
-cdf(){ cd $(dirname $1) }
-cdfile(){ cdf $1}
