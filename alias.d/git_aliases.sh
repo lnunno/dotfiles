476b0dc88c709bd584db782234615aec9d8cae46
Lucas Nunno <lucasnunno@gmail.com>
2017-04-12 18:12:12 -0600

    Update and rename git_aliases.zsh to git_aliases.sh

--- alias.d/git_aliases.sh
+++ alias.d/git_aliases.sh
@@ -16,5 +16,5 @@ alias gpo='git push --set-upstream origin'
 alias gporigin='git push --set-upstream origin'
 alias git-prune-branches='git branch --merged | grep -v "\*" | grep -Ev "(\*|master|develop|staging)" | xargs -n 1 git branch -d'
 
-# Git push my branch to origin's branch of the same name.
+# Git push branch to origin's branch of the same name.
 alias git-push-upstream='git push --set-upstream origin $(git symbolic-ref --short HEAD)'
