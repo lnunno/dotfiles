cdb290705aebe38284c6707af66abcf7185858bc
Lucas Nunno <lucasnunno@gmail.com>
2017-07-17 14:23:10 -0600

    Update docker_aliases.sh

--- alias.d/docker_aliases.sh
+++ alias.d/docker_aliases.sh
@@ -1,7 +1,9 @@
+# Aggregate command aliases for cleaning out space
 alias docker-clean-exited-containers='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
 alias docker-clean-dangling-images='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
 alias docker-delete-all-images='docker rmi $(docker images -q) -f'
 alias docker-clean-all-images='docker-delete-all-images'
+alias docker-clean-dangling-volumes='docker volume rm $(docker volume ls -f dangling=true -q)'
 
 alias dck='docker'
 alias di='docker images'
@@ -12,7 +14,13 @@ alias dpsa='docker ps -a'
 alias drm='docker rm'
 alias drmi='docker rmi'
 
+# Exec/run aliases
 alias dex='docker exec'
 alias dexit='docker exec -it'
 alias drun='docker run'
 alias drunit='docker run -it'
+
+# Volume aliases
+alias dv='docker volume'
+alias dvls='docker volume ls'
+alias dvrm='docker volume rm'
