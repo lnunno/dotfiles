541f4dbb414515c7c8df80282989bc61fb12738e
Lucas Nunno <lucasnunno@gmail.com>
2017-12-03 23:52:48 -0700

    Update cdf command to follow symlinks.

--- alias.d/aliases.sh
+++ alias.d/aliases.sh
@@ -4,7 +4,16 @@ alias abs='realpath'
 alias abspath='realpath'
 alias mysudo='sudo env PATH=$PATH'
 
-cdf(){ cd $(dirname $1) }
+cdf(){
+    linkdir="$(readlink $1)"
+    if [ -n "$linkdir" ]; then
+        echo $linkdir
+        cd $(dirname ${linkdir})
+    else
+        cd $(dirname $1)
+    fi
+}
+
 cdfile(){ cdf $1}
 
 # See: http://stackoverflow.com/questions/749544/pipe-to-from-the-clipboard
