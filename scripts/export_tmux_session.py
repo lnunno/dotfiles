148f11ecae177c8522843636e5c9896430a45b73
Lucas Nunno <lucasnunno@gmail.com>
2019-01-15 21:03:43 -0800

    Export tmux session script.

--- scripts/export_tmux_session.py
+++ scripts/export_tmux_session.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+"""Export the current tmux session as a tmuxinator YAML file.
+"""
 import subprocess
 import yaml
 
@@ -6,7 +8,7 @@ def main():
     output = subprocess.check_output(['tmux', 'list-panes', '-s', '-F', "#{session_name} #{window_index} #{window_name} #{pane_index} #{pane_current_path}"], encoding='utf8')
     yaml_dict = {}
     lines = output.splitlines()
-    session_name, window_index, window_name, pane_index, pane_current_path = lines[0].split()
+    session_name, _, _, _, _ = lines[0].split()
     windows = []
     yaml_dict['name'] = session_name
     yaml_dict['root'] = '~/'
